c4a500c54db1076f5a8148b97e23b5cba21cd69b
Stef Claus <stef.claus@gmail.com>
2019-04-11 19:49:14 +0000

    added users and workouts migration details

---
+++ db/migrate/02_create_workouts.rb
@@ -0,0 +1,14 @@
+class CreateWorkouts < ActiveRecord::Migration[5.2]
+  def up
+    create_table :workouts do |t|
+      t.string :workout
+      t.string :description
+      t.integer :duration
+    end
+  end
+
+  def down
+    drop_table :tweets
+  end
+
+end
